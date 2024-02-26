9c3c7011251bc9b02f5f402621cf2a4a62607734
Kakanang Thongsin <kakanang.tho@outlook.com>
2024-02-26 14:53:09 +0700

    Change path

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/exception/ApiExceptionHandler.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/exception/ApiExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.exercise.wallets.exception;
+package exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
--- posttest/src/main/java/com/kbtg/bootcamp/posttest/exception/DuplicateException.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/exception/DuplicateException.java
@@ -1,4 +1,4 @@
-package com.exercise.wallets.exception;
+package exception;
 
 public class DuplicateException extends RuntimeException{
     public DuplicateException(String message) {
